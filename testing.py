87d9e4a067041ff9c75390b363223171384aa854
Gavin Wilson <gaqw0210@gmail.com>
2023-06-10 13:01:33 -0700

    Removed a testing file for overlap.py

--- testing.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def generate_test_pairs(length=10, overlap=5):
-    list1 = random.sample(range(1, length * 2), length)
-    common_values = random.sample(list1, overlap)
-    list2 = random.sample(common_values + list(set(range(1, length * 2)) - set(list1)), length)
-    return list1, list2
